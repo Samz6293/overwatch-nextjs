c52b6a36ffd3d514575f6369372d2356ac2f3d3c
dh1-wsl <237053497+Samz6293@users.noreply.github.com>
2026-09-20 08:02:36 +0600

    Add Hero

---
+++ app/components/Hero.tsx
@@ -0,0 +1,45 @@
+
+export default function Hero() {
+    return (
+        <div className="bg-[url(/moira.jpg)] bg-no-repeat bg-position-[75%_top] bg-cover text-white h-[80vh]
+        md:bg-top">
+            {/* Mobile readability scrim: active on small screens, transparent on desktop */}
+            <div className="h-full w-full bg-linear-to-t from-black/95 via-black/80 to-transparent
+            md:bg-transparent">
+                <div className="content-box h-full flex flex-col justify-center gap-3 px-6
+                sm:gap-6 sm:px-12
+                md:px-16">
+                    <h1 className="text-2xl text-start tracking-tight leading-7 font-monument
+                    sm:text-4xl sm:leading-10
+                    md:text-5xl md:leading-11">
+                        PICK A HERO.<br />
+                        CREATE CHAOS.
+                    </h1>
+
+                    <p className="text-start font-apple max-w-100 text-neutral-200
+                    sm:text-md md:text-xl md:text-white">
+                        Step into a team-based action game where every hero plays by their own rules.
+                        Team up with your friends and battle for the objective
+                    </p>
+
+                    <div className="flex gap-3 pt-2
+                    sm:gap-4 sm:pt-0">
+
+                        <button className="bg-orange-500 px-5 py-2 rounded-lg font-monument text-[10px] cursor-pointer
+                        sm:px-6 sm:text-sm
+                        hover:bg-orange-600 transition-colors">
+                            Heroes
+                        </button>
+
+                        <button className="border border-orange-500 px-5 py-2 rounded-lg font-monument text-[10px] cursor-pointer
+                        sm:px-6 sm:text-sm
+                        hover:bg-orange-500/10 transition-colors">
+                            Try Overwatch
+                        </button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
+import Hero from "./components/Hero";
 
 export default function Home() {
     return (
         <>
-            <div className="flex flex-col gap-2 p-6">
-                <h1 className={`text-4xl text-start tracking-tight leading-8 font-monument`}>OVERWATCH <br />
-                THE WORLD NEEDS HEROES.</h1>
-                <h1 className={`text-xl text-start font-apple`}>Step into a team-based action game where every hero plays by their own rules. <br />
-                Choose your hero. Master their abilities. Team up with your friends and battle for the objective in fast, chaotic, unforgettable matches.</h1>
-            </div>
+            <Hero />
+            <div className="h-100"></div>
         </>
     );
 }
