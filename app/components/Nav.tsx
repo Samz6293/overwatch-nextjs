3f3895cf2a470d5e54e38c677430233da53c9291
dh1-Omarchy <237053497+Samz6293@users.noreply.github.com>
2026-09-21 08:48:46 +0600

    Update Nav

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,5 +1,9 @@
+"use client";
+import Link from "next/link"
 import { FaGithub } from "react-icons/fa"
-import { MdMenu } from "react-icons/md"
+import { MdClose, MdMenu } from "react-icons/md"
+import { navLinks } from "../constants"
+import Image from "next/image";
 
 const Nav = () => {
     return (
@@ -7,30 +11,32 @@ const Nav = () => {
         sm:text-xs lg:text-sm">
             <div className="content-box-s grid grid-cols-2  items-center p-3 
             sm:content-box md:grid-cols-3">
-                <MdMenu className="text-xl md:hidden"/>
+                <button className="text-xl md:hidden"><MdMenu/></button>
+                <button className="text-xl hidden"><MdClose/></button>
 
-                <div className="justify-center hidden  
-                md:flex md:justify-start">
-                    <img className="block ml-15 h-6 sm:h-7 md:ml-0 lg:h-8" src="/favicon.ico" alt="Dev Stack Logo" />
+                <div className="justify-start hidden  
+                md:flex">
+                    <Image src="/overwatch-logo.png" alt="Overwatch Logo" width={757} height={122} className=" h-[40px]"/>
                 </div>
 
                 <ul className="hidden text-white justify-center gap-9 font-apple text-lg
                 md:flex">
-                    <li className="nav-hover"><a href="#">Roles</a></li>
-                    <li className="nav-hover"><a href="#">Heroes</a></li>
-                    <li className="nav-hover"><a href="#">About</a></li>
+                    {navLinks.map(link =>
+                        <li key={link.id} className="nav-hover"><Link href={link.id}>{link.title}</Link></li>
+                    )}
                 </ul>
 
                 <div className="flex justify-end">
 
-                <a href="https://github.com/Samz6293/overwatch-nextjs" target="_blank" rel="noreferrer"
+
+                <Link href="https://github.com/Samz6293/overwatch-nextjs" target="_blank" rel="noreferrer"
                 className="inline-flex items-center gap-2 px-3.5 py-1.5 rounded-full text-xs font-mono font-semibold
               bg-white/5 border border-white/10 text-gray-200
               hover:bg-orange-600 hover:text-white hover:border-orange-500 hover:shadow-[0_0_15px_rgba(220,38,38,0.4)]
                 active:scale-95 transition-all duration-200">
                     <FaGithub className="text-2xl" />
                     <span className="text-sm">Source</span>
-                </a>
+                </Link>
                 </div>
             </div>
         </nav>
---
+++ app/constants/index.ts
@@ -0,0 +1,5 @@
+export const navLinks = [
+    {title: "Roles", id: "roles"},
+    {title: "Heroes", id: "heroes"},
+    {title: "About", id: "about"},
+]
